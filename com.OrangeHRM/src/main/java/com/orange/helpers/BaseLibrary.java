a69d282c782aee1abd900ded1c542bfe24bdbf80
nikita <nikita@DELTADESIGNS>
2021-06-21 18:18:07 +0530

    test

--- com.OrangeHRM/src/main/java/com/orange/helpers/BaseLibrary.java
+++ com.OrangeHRM/src/main/java/com/orange/helpers/BaseLibrary.java
@@ -34,6 +34,8 @@ import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseLibrary {
+	
+	// test
 	public static WebDriver driver;
 	public final int IMPLICIT_TIMEOUT = 10;
 	public String baseURL;
